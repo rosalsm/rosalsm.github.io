9af98c516d41c57ed8590b4583b6595f4c434b6b
rosa <r215130@gmail.com>
2015-10-22 13:47:09 -0400

    working in the tab

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 
-$('#who').on('click', function(){
+$('.who').on('click', function(){
   console.log('funciona');
  $(this).addClass('active').siblings().removeClass('active');
  $('div.tab-who').addClass('active').siblings().removeClass('active');
 })
-$('#journals').on('click', function(){
+$('.journals').on('click', function(){
   console.log('funciona journals');
   $(this).addClass('active').siblings().removeClass('active');
   $('div.tab-journals').addClass('active').siblings().removeClass('active');
