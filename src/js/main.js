aed345e577f3cf44ca6b55cad5de98c8a78570e3
rosa <r215130@gmail.com>
2016-01-02 08:21:11 -0500

    functionality

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,18 @@ $('.fa-bars').on('click', function(){
   $('.navigator').addClass('show-nav').removeClass('navigator');
   $('.fa-bars').addClass('no-hamburger').removeClass('fa-bars');
   console.log("hamburger test")
-})
+});
 
+$('.navigator a[href="#portfolio"]').on('click', function(){
+  $('html,body').animate({scrollTop: $('#portfolio').offset().top}, 'slow');
+});
 
+$('.navigator a[href="#about"]').on('click', function(){
+  $('html,body').animate({scrollTop: $('#about').offset().top}, 'slow');
+});
+
+$('.navigator a[href="#contact"]').on('click', function(){
+  $('html,body').animate({scrollTop: $('#contact').offset().top}, 'slow');
+});
 
 })(); //END OF IIFE
-
-
-// $('i.fa-bars').on('click', function(){
-//           $('.tabs-header').addClass('show-header').removeClass('tabs-header');
-//           console.log("changing header")
