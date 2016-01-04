8f0db53318fe98cd56775d6d9a17517027dc6816
rosa <r215130@gmail.com>
2016-01-04 12:35:23 -0500

    finishing functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,31 @@
 ;(function(){
+  // $('.fa-bars').on('click', function(){
+  //   $('nav.navigator').toggleClass('show-nav').toggleClass('navigator');
+  //   $('i.fa-bars').toggleClass('no-hamburger').toggleClass('fa-bars');
+  //   console.log("hamburger test")
+  // });
 
+
+
+//WHEN CLICK HAMBURGER, WE SEE THE MENU
 $('.fa-bars').on('click', function(){
   $('.navigator').addClass('show-nav').removeClass('navigator');
   $('.fa-bars').addClass('no-hamburger').removeClass('fa-bars');
   console.log("hamburger test")
 });
 
+//WHEN WE CLICK THE MENU, WE COME BACK TO HAMBURGER
+$('nav a').on('click', function(){
+  $('.show-nav').addClass('navigator').removeClass('show-nav');
+  $('.no-hamburger').addClass('fa-bars').removeClass('no-hamburger');
+  console.log("testing back")
+
+});
+
+
+
+
+//SCROLL THE PAGE TO GO TO A DIFFERENT SECTION
 $('.navigator a[href="#portfolio"]').on('click', function(){
   $('html,body').animate({scrollTop: $('#portfolio').offset().top}, 'slow');
 });
